2a2f2141730465df104eb9ab4101bafbe8df7e2c
Lukasz <53575849+Lukasz7h@users.noreply.github.com>
2022-12-13 17:35:03 +0100

    test v1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,6 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
@@ -11,25 +13,17 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      schemas: [
+        NO_ERRORS_SCHEMA
+      ]
     }).compileComponents();
   });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+
+    console.log(app)
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'speech_to_text'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('speech_to_text');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('speech_to_text app is running!');
   });
 });
--- src/app/notes/notes.component.spec.ts
+++ src/app/notes/notes.component.spec.ts
@@ -20,4 +20,18 @@ describe('NotesComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should had padding elements', () => {
+    const compiled = fixture.nativeElement;
+    expect(compiled.getElementsByClassName("padding").length).toEqual(4);
+  })
+
+  // object settings test
+  it('should had paddings', () => {
+    const padding = component.settings.padding;
+    for(let e in padding)
+    {
+      expect(padding[`${e}`]).toBeGreaterThanOrEqual(0);
+    };
+  });
 });
